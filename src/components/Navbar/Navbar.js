17f0534e74cef70865e5e9333a2dcd7539eeec6b
Vinod Kumar R <47006421+vk0808@users.noreply.github.com>
2021-07-02 21:31:40 +0530

    included react-icons, modified structure

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,15 +1,38 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
+import { FaBars } from "react-icons/fa";
+
 import { ReactComponent as Logo } from "./logo.svg";
 import "../../styles.scss";
 
 const Navbar = () => {
+  const [showLinks, setShowLinks] = useState(false);
+  const linksContainerRef = useRef(null);
+  const linksRef = useRef(null);
+
+  useEffect(() => {
+    const linksHeight = linksRef.current.getBoundingClientRect().height;
+    if (showLinks) {
+      linksContainerRef.current.style.height = `${linksHeight + 65}px`;
+    } else {
+      linksContainerRef.current.style.height = "0px";
+    }
+  }, [showLinks]);
+
   return (
     <>
       <nav>
         <div className="nav-center">
-          <Logo className="logo" />
-          <div className="nav-menu">
-            <ul>
+          <div className="nav-header">
+            <Logo className="logo" />
+            <button
+                className="nav-toggle"
+                onClick={() => setShowLinks(!showLinks)}
+              >
+                <FaBars />
+              </button>
+          </div>
+          <div className="nav-menu links-container" ref={linksContainerRef}>
+            <ul className="links" ref={linksRef}>
               <li>
                 <a className="nav-link" href="#problem">
                   problem
