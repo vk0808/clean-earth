f93fcd7ebb738ff4a5c2a7e01e50e88f49fa21bb
Vinod Kumar R <47006421+vk0808@users.noreply.github.com>
2021-07-02 19:23:26 +0530

    added nav menu

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,9 +1,25 @@
 import React from "react";
+import { ReactComponent as Logo } from "./logo.svg";
 
 const Navbar = () => {
   return (
     <>
-      <p>Navbar</p>
+      <nav>
+        <div className="nav-center">
+          <Logo className="logo" />
+          <div className="nav-menu">
+            <a className="nav-link" href="#problem">
+              problem
+            </a>
+            <a className="nav-link" href="#solution">
+              solution
+            </a>
+            <button className="pledge" type="button">
+              take pledge
+            </button>
+          </div>
+        </div>
+      </nav>
     </>
   );
 };
