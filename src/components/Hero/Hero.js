30b1fd8e674fe7f5ffea8157a9da0cd5ca4b29d3
Vinod Kumar R <47006421+vk0808@users.noreply.github.com>
2021-07-05 22:23:08 +0530

    added hero content

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -2,9 +2,23 @@ import React from "react";
 
 const Hero = () => {
   return (
-    <>
-      <p>Hero</p>
-    </>
+    <section className="hero">
+      <div className="left"></div>
+      <div className="right">
+        <h1 className="hero-title">think before throwing waste</h1>
+        <p className="hero-desc">
+          The total MSW generated in urban India has been estimated at
+          <strong> 68.8 million tons per year</strong>, out of which about
+          <strong> 40% </strong> is not collected at all, littering in the
+          city/town and finds its way to nearby drains and water bodies. Take a
+          pledge to reduce waste by incorporating
+          <strong> 5 R principle</strong>.
+        </p>
+        <button type="button" className="pledge">
+          Take pledge
+        </button>
+      </div>
+    </section>
   );
 };
 
