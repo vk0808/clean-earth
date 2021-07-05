fad2c80b43a081f43848ac5fce1738369ecb54db
Vinod Kumar R <47006421+vk0808@users.noreply.github.com>
2021-07-05 21:35:28 +0530

    added hero bolierplate, bg-img

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <>
+      <p>Hero</p>
+    </>
+  );
+};
+
+export default Hero;
--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "../components/Navbar/Navbar";
+import Hero from "../components/Hero/Hero";
 
 const Index = () => {
   return (
     <>
       <Navbar />
+      <Hero />
     </>
   );
 };
