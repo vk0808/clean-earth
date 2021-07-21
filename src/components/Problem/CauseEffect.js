9ca19aa54b6a0f404f838fbc5571830a9a1ef289
Vinod Kumar R <47006421+vk0808@users.noreply.github.com>
2021-07-21 17:09:09 +0530

    added: info json and cause content

--- src/components/Problem/CauseEffect.js
+++ src/components/Problem/CauseEffect.js
@@ -1,9 +1,24 @@
 import React from "react";
 
+import { cause } from "./info";
+
 const CauseEffect = () => {
   return (
     <>
-      <p>Problem Cause and Effect</p>
+      <section className="cause">
+        <h3 className="sub-heading">The Cause</h3>
+        <ol className="listing">
+          {cause.map((item) => {
+            const { id, title, description } = item;
+            return (
+              <li key={id}>
+                <h3>{title}</h3>
+                <p className="cause-desc">{description}</p>
+              </li>
+            );
+          })}
+        </ol>
+      </section>
     </>
   );
 };
---
+++ src/components/Problem/info.js
@@ -0,0 +1,22 @@
+export const cause = [
+  {
+    id: 1,
+    title: "The amount of waste is simply too big",
+    description: "With the growing population, waste generation will most likely to increase from 68.8 million tons to about 165 million tons in 2030.",
+  },
+  {
+    id: 2,
+    title: "Landfills are a problem as well",
+    description: "The problems with landfill are toxins, leachate and greenhouse gases, caused by chemical, biological or industrial processes. Typical household articles that may result in toxic waste include pesticides, computers, cell phones and batteries.",
+  },
+  {
+    id: 3,
+    title: "Public vs. private interest",
+    description: "Private companies try to maximize profits and usually don’t care too much about adverse negative impact on the environment, while it is in the public interest to save and conserve environment. Even governments are largely influenced by the big companies.",
+  },
+  {
+    id: 4,
+    title: "Ineffective long-term waste disposal strategies",
+    description: "Many cities are simply overwhelmed and are not able to manage trash in an appropriate manner. Moreover, technologies for waste disposal are far less advanced.",
+  },
+]
