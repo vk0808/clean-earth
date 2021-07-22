c39b7627f46d560d10303444137020de450382d3
Vinod Kumar R <47006421+vk0808@users.noreply.github.com>
2021-07-22 22:34:43 +0530

    added: btn-hover effect

--- src/components/Problem/List.js
+++ src/components/Problem/List.js
@@ -1,19 +1,19 @@
-import React, { useState } from 'react';
-import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
+import React, { useState } from "react";
+import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
 
 const List = ({ title, description }) => {
   const [showInfo, setShowInfo] = useState(false);
   return (
-    <article className='list'>
+    <article className="list">
       <header>
         <h4>{title}</h4>
-        <button className='btn' onClick={() => setShowInfo(!showInfo)}>
+        <button className="btn" onClick={() => setShowInfo(!showInfo)}>
           {showInfo ? <AiOutlineMinus /> : <AiOutlinePlus />}
         </button>
       </header>
       {showInfo && <p>{description}</p>}
     </article>
-  )
-}
+  );
+};
 
 export default List;
