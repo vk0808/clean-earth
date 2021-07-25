59dfd0d8f2bf473036a16a547edb3d551c85dd45
Vinod Kumar R <47006421+vk0808@users.noreply.github.com>
2021-07-25 19:43:11 +0530

    added: footer logo, social-links

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,30 @@
 import React from "react";
+import { FaFacebook, FaTwitter } from "react-icons/fa";
+import { GrInstagram } from "react-icons/gr";
+
+import { ReactComponent as Logo } from "../Navbar/logo.svg";
 
 const Footer = () => {
   return (
     <footer>
       <div className="footer-img"></div>
       <div className="footer-content">
-        <p>footer</p>
+        <div className="footer-left">
+          <Logo className="logo" />
+        </div>
+        <div className="footer-right">
+          <ul className="social-links">
+            <li>
+              <FaFacebook />
+            </li>
+            <li>
+              <FaTwitter />
+            </li>
+            <li>
+              <GrInstagram />
+            </li>
+          </ul>
+        </div>
       </div>
     </footer>
   );
