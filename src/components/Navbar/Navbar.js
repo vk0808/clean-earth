d246f4f39852240d0015639d9d49005313e9578c
Vinod Kumar R <47006421+vk0808@users.noreply.github.com>
2021-07-25 23:12:42 +0530

    added: sticky navbar with style

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,14 +1,25 @@
 import React, { useState, useEffect, useRef } from "react";
 import { FaBars } from "react-icons/fa";
-import {Link} from 'react-scroll'
+import { Link } from "react-scroll";
 
 import { ReactComponent as Logo } from "./logo.svg";
 import "../../styles.scss";
 
 const Navbar = () => {
   const [showLinks, setShowLinks] = useState(false);
+  const [navbar, setNavbar] = useState(false);
   const linksContainerRef = useRef(null);
   const linksRef = useRef(null);
+
+  const changeNavbarBg = () => {
+    if (window.scrollY >= 60) {
+      setNavbar(true);
+    } else {
+      setNavbar(false);
+    }
+  };
+
+  window.addEventListener("scroll", changeNavbarBg);
 
   useEffect(() => {
     const linksHeight = linksRef.current.getBoundingClientRect().height;
@@ -21,7 +32,7 @@ const Navbar = () => {
 
   return (
     <>
-      <nav>
+      <nav className={navbar ? "navbar nav-active" : "navbar"}>
         <div className="nav-center">
           <div className="nav-header">
             <Logo className="logo" />
@@ -35,12 +46,22 @@ const Navbar = () => {
           <div className="nav-menu links-container" ref={linksContainerRef}>
             <ul className="links" ref={linksRef}>
               <li>
-                <Link className="nav-link" to="problem" spy={true} smooth={true}>
+                <Link
+                  className="nav-link"
+                  to="problem"
+                  spy={true}
+                  smooth={true}
+                >
                   problem
                 </Link>
               </li>
               <li>
-                <Link className="nav-link" to="solution" spy={true} smooth={true}>
+                <Link
+                  className="nav-link"
+                  to="solution"
+                  spy={true}
+                  smooth={true}
+                >
                   solution
                 </Link>
               </li>
