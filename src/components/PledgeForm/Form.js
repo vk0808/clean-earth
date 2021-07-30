1ea70ed165459eeca0d408ed283e587684cadc87
Vinod Kumar R <47006421+vk0808@users.noreply.github.com>
2021-07-30 13:47:54 +0530

    added: pledgeForm boilerplates

---
+++ src/components/PledgeForm/Form.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Form = () => {
+  return (
+    <>
+      <p>Form</p>
+    </>
+  );
+};
+
+export default Form;
--- src/pages/Pledge.js
+++ src/pages/Pledge.js
@@ -1,12 +1,14 @@
 import React from "react";
 
 import Navbar from "../components/Navbar/Navbar";
+import Form from "../components/PledgeForm/Form";
 import Footer from "../components/Footer/Footer";
 
 const Pledge = () => {
   return (
     <>
       <Navbar />
+      <Form />
       <Footer />
     </>
   );
